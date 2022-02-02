a7515706f65e0ef2a5658d594956a19c1603e813
Artur Rudenko <RudenkoArt@outlook.com>
2022-02-02 07:36:24 +0200

    fix tests for capitalize.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,15 +1,8 @@
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('nike') !== 'Nike') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('nike'), 'Nike');
 
 console.log('Все тесты пройдены!');
